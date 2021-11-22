3e1b3f9a2332924edda475d8afe6698edd0eb0b5
Muhammad Hamid Raza <68273560+hamidrazabutt@users.noreply.github.com>
2021-11-23 04:27:33 +0500

    Update index.js

--- 02-minimal-node-application/src/index.js
+++ 02-minimal-node-application/src/index.js
@@ -13,58 +13,58 @@ const client = new ApolloClient({
   },
 });
 
-// React => Axios => Github gql
-// Node => Apollo-boost => Github gql
+React => Axios => Github gql
+Node => Apollo-boost => Github gql
 
-// const GET_ORG_DETAILS = gql`
-//   query {
-//     organization(login: "qutbITech") {
-//       name
-//       id
-//     }
-//   }
-// `;
+const GET_ORG_DETAILS = gql`
+  query {
+    organization(login: "qutbITech") {
+      name
+      id
+    }
+  }
+`;
 
-// // POST
-// client.query({
-//   query: GET_ORG_DETAILS
-// }).then(res => console.log('GET_ORG_DETAILS >>>', res));
+// POST
+client.query({
+  query: GET_ORG_DETAILS
+}).then(res => console.log('GET_ORG_DETAILS >>>', res));
 
-// Data Scraping
-// AI
+Data Scraping
+AI
 
-// Server => Request (daraz => Mobile Price) => Database  ???? (Client/React)
+Server => Request (daraz => Mobile Price) => Database  ???? (Client/React)
 
-// React => Server => (Database) => Response Client (UI)
+React => Server => (Database) => Response Client (UI)
 
-// // Query with Variables
+// Query with Variables
 
-// const GET_ORG_DETAILS_VARIABLE = gql`
-//   query getOrganization($org: String!, $org2: String!){
-//     org1: organization(login: $org) {
-//       name
-//       url
-//       description
-//     }
-//     org2: organization(login: $org2) {
-//       name
-//       url
-//       description
-//     }
-//   }
-// `;
+const GET_ORG_DETAILS_VARIABLE = gql`
+  query getOrganization($org: String!, $org2: String!){
+    org1: organization(login: $org) {
+      name
+      url
+      description
+    }
+    org2: organization(login: $org2) {
+      name
+      url
+      description
+    }
+  }
+`;
 
-// client.query({
-//   query: GET_ORG_DETAILS_VARIABLE,
-//   variables: {
-//     org: "qutbITech",
-//     org2: "facebook"
-//   }
-// })
-// .then(res => console.log('GET_ORG_DETAILS_VARIABLE >>>', res))
-// .catch(err => console.log('Err: ', err));
+client.query({
+  query: GET_ORG_DETAILS_VARIABLE,
+  variables: {
+    org: "qutbITech",
+    org2: "facebook"
+  }
+})
+.then(res => console.log('GET_ORG_DETAILS_VARIABLE >>>', res))
+.catch(err => console.log('Err: ', err));
 
-// // MUTATION
+// MUTATION
 
 const ADD_STAR = gql`
   mutation AddStartToMyRepo($repoId: ID!) {
