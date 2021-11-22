d0e82426f8b26ce984f6ca4536bc957e5683c610
Muhammad Hamid Raza <68273560+hamidrazabutt@users.noreply.github.com>
2021-11-22 06:53:45 +0500

    Done Steps3

---
+++ graphql-demo1/src/GithubGraphQL.js
@@ -0,0 +1,97 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'REACT_APP_BASEURL', // Github (You can get from .env file or Direct put here:'https://api.github.com',)
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`, // access token
+  },
+});
+
+function GithubAPI() {
+  //   const fetchData_js = async organizationName => {
+  //     const QUERY_ORGANIZATION = `query {
+  //             organization(login: "${organizationName}") {
+  //                 name
+  //                 description
+  //                 url
+  //                 createdAt
+  //             }
+  //             viewer {
+  //                 login
+  //                 email
+  //                 company
+  //                 repositories(first: 5) {
+  //                     edges {
+  //                         node {
+  //                             name
+  //                         }
+  //                     }
+  //                 }
+  //             }
+  //         }`;
+
+  //     const res = await api.post('/graphql', { query: QUERY_ORGANIZATION });
+  //     console.log('GraphQL Response: ', res);
+  //   };
+
+  const fetchData_js = async organizationName => {
+    const QUERY_ORGANIZATION = `query queryOrganization($organizationName: String!){
+              organization(login: $organizationName) {
+                  name
+                  description
+                  url
+                  createdAt
+              }
+              viewer {
+                  login
+                  email
+                  company
+                  repositories(first: 5) {
+                      edges {
+                          node {
+                              name
+                          }
+                      }
+                  }
+              }
+          }`;
+
+    const res = await api.post('/graphql', {
+      query: QUERY_ORGANIZATION,
+      variables: {
+        organizationName,
+      },
+    });
+    console.log('GraphQL Response: ', res);
+  };
+
+  const addStart = async repositoryId => {
+    const MUTATION_ADD_START = `mutation AddStarToMyRepo($organizationName: ID!) {
+            addStar(input: {starrableId: $organizationName}) {
+                starrable {
+                    viewerHasStarred
+                }
+            }
+        }`;
+
+    const {
+      data: { data },
+    } = await api.post('/graphql', {
+      variables: {
+        abc: repositoryId,
+      },
+      query: MUTATION_ADD_START,
+    });
+    console.log('GraphQL Response: ', data);
+  };
+
+  useEffect(() => {
+    fetchData_js('qutbITech');
+    //   addStart('MDEwOlJlcG9zaXRvcnkzODc1MTIzNjA=');
+  }, []);
+
+  return <div></div>;
+}
+
+export default GithubAPI;
