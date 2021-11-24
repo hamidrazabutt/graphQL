9d06a4d91d8103dbd7d016662868e6f17fbabef7
Muhammad Hamid Raza <68273560+hamidrazabutt@users.noreply.github.com>
2021-11-25 00:35:11 +0500

    Class 14

--- 04-node-graplQL-application/src/Schema/Resolvers.js
+++ 04-node-graplQL-application/src/Schema/Resolvers.js
@@ -1,34 +1,30 @@
-import {users} from '../Mock_Data'
+import { users } from '../Mock_Data';
 import { pubsub } from './index';
 
 export const resolvers = {
-    Query: {
-        getAllUsers() {
-            return users;
-        }
+  Query: {
+    getAllUsers() {
+      return users;
     },
-    Mutation: {
-        createUser(parent, args) {
-            const newUser = args;
-            users.push(newUser);
-            pubsub.publish('TRIGGER_NEW_USER', { newUser });
-            return newUser
-        },
-        deleteUser(parent, args) {
-            const deletedUser = users.find(({ name }) => name == args.name)
-            console.log(deletedUser)
-            // delete user logic from an Array <users>
-            pubsub.publish('TRIGGER_DELETED_USER', { deletedUser });
-            return deletedUser
-        }
+  },
+  Mutation: {
+    createUser(parent, args) {
+      const newUser = args;
+      users.push(newUser);
+      pubsub.publish('TRIGGER_NEW_USER', { newUser });
+      return newUser;
     },
-    Subscription: {
-        newUser: {
-            subscribe: () => pubsub.asyncIterator(['TRIGGER_NEW_USER'])
-        },
-        deletedUser: {
-            subscribe: () => pubsub.asyncIterator(['TRIGGER_DELETED_USER'])
-        }
-    }
-
+    deleteUser(parent, args) {
+      const deletedUser = users.find(({ name }) => name == args.name);
+      console.log(deletedUser);
+      // delete user logic from an Array <users>
+      pubsub.publish('TRIGGER_DELETED_USER', { deletedUser });
+      return deletedUser;
+    },
+  },
+  Subscription: {
+    newUser: {
+      subscribe: () => pubsub.asyncIterator(['TRIGGER_NEW_USER']),
+    },
+  },
 };
