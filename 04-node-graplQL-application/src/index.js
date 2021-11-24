27dc5f72ae7cf6f872332ce9b2fd7b93d74b53ba
Muhammad Hamid Raza <68273560+hamidrazabutt@users.noreply.github.com>
2021-11-24 05:34:06 +0500

    class 14

--- 04-node-graplQL-application/src/index.js
+++ 04-node-graplQL-application/src/index.js
@@ -1,12 +1,12 @@
 import 'dotenv/config';
-import { createServer } from "http";
-import express from "express";
-import { execute, subscribe } from "graphql";
-import { ApolloServer, gql } from "apollo-server-express";
+import { createServer } from 'http';
+import express from 'express';
+import { execute, subscribe } from 'graphql';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-import { SubscriptionServer } from "subscriptions-transport-ws";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-import { typeDefs, resolvers } from './Schema'
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { typeDefs, resolvers } from './Schema';
 
 const PORT = process.env.PORT;
 const app = express();
@@ -15,46 +15,92 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 const server = new ApolloServer({ schema });
 
 (async () => {
-    
-    await server.start();
-    server.applyMiddleware({ app });
+  await server.start();
+  server.applyMiddleware({ app });
 
-    SubscriptionServer.create(
-        { schema, execute, subscribe },
-        { server: httpServer, path: server.graphqlPath }
+  SubscriptionServer.create(
+    { schema, execute, subscribe },
+    { server: httpServer, path: server.graphqlPath },
+  );
+
+  httpServer.listen(PORT, () => {
+    console.log(
+      `🚀 Query endpoint ready at http://localhost:${PORT}${server.graphqlPath}`,
     );
-
-    httpServer.listen(PORT, () => {
-        console.log(
-            `🚀 Query endpoint ready at http://localhost:${PORT}${server.graphqlPath}`
-        );
-        console.log(
-            `🚀 Subscription endpoint ready at ws://localhost:${PORT}${server.graphqlPath}`
-        );
-    });
-
+    console.log(
+      `🚀 Subscription endpoint ready at ws://localhost:${PORT}${server.graphqlPath}`,
+    );
+  });
 })();
 
-
-
 // My Rest APIs (-SAMPLE-) :)
-app.get('/', (req, res)=> {
-    // Endpoint: http://localhost:4000/
-    res.json({
-        graphQL: `http://localhost:${PORT}${server.graphqlPath}`,
-        subscription: `ws://localhost:${PORT}${server.graphqlPath}`
-    })
-})
+app.get('/', (req, res) => {
+  // Endpoint: http://localhost:4000/
+  res.json({
+    graphQL: `http://localhost:${PORT}${server.graphqlPath}`,
+    subscription: `ws://localhost:${PORT}${server.graphqlPath}`,
+  });
+});
 
-app.get('/github/:name', (req, res)=> {
-    // Endpoint: http://localhost:4000/github/uqutub
-    res.json({
-        username: req.params.name,
-        githutb: `https://api.github.com/users/${req.params.name}`,
-    })
-})
+app.get('/github/:name', (req, res) => {
+  // Endpoint: http://localhost:4000/github/uqutub
+  res.json({
+    username: req.params.name,
+    githutb: `https://api.github.com/users/${req.params.name}`,
+  });
+});
 
-app.get('/queryString', (req, res)=> {
-    // Endpoint: http://localhost:4000/queryString?name=uqutub&email=usuf52@gmail.com
-    res.json(req.query)
-})
+app.get('/queryString', (req, res) => {
+  // Endpoint: http://localhost:4000/queryString?name=uqutub&email=usuf52@gmail.com
+  res.json(req.query);
+});
+
+// const { ApolloServer, gql } = require('apollo-server');
+
+// // A schema is a collection of type definitions (hence "typeDefs")
+// // that together define the "shape" of queries that are executed against
+// // your data.
+// const typeDefs = gql`
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+//   # This "Book" type defines the queryable fields for every book in our data source.
+//   type Book {
+//     title: String
+//     author: String
+//   }
+
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     books: [Book]
+//   }
+// `;
+
+// const books = [
+//   {
+//     title: 'The Awakening',
+//     author: 'Kate Chopin',
+//   },
+//   {
+//     title: 'City of Glass',
+//     author: 'Paul Auster',
+//   },
+// ];
+
+// // Resolvers define the technique for fetching the types defined in the
+// // schema. This resolver retrieves books from the "books" array above.
+// const resolvers = {
+//   Query: {
+//     books: () => books,
+//   },
+// };
+
+// // The ApolloServer constructor requires two parameters: your schema
+// // definition and your set of resolvers.
+// const server = new ApolloServer({ typeDefs, resolvers });
+
+// // The `listen` method launches a web server.
+// server.listen().then(({ url }) => {
+//   console.log(`🚀  Server ready at ${url}`);
+// });
